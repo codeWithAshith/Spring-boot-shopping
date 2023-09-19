00248c745e64605b12876419c712c4305844f54e
codewithashith <codewithashith@gmail.com>
2023-09-19 18:03:05 +0530

    Initial commit

---
+++ src/test/java/com/codewithashith/springbootshopping/SpringBootShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithashith.springbootshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
